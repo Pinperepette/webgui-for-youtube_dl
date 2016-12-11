f8fa8637597f2c215f83786f68c24bce0a197134
Pinperepette <pinperepette@gmail.com>
2016-12-11 13:48:23 +0100

    Upload file

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import youtube_dl
+
+app = Flask(__name__)
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+}
+
+@app.route("/", methods=['GET', 'POST'])
+def calculation():
+    result = 0
+    error = ''
+
+    if request.method=='POST':
+        url = request.form['first']
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+            error = 'fatto'
+    return render_template('pinpegui.html',error=error)
+
+if __name__ == "__main__":
+    app.run()
